aeadd2d9c9abbd5fcb5412b186dfb6fd3542660b
driehoeken <a.bednarski1203@gmail.com>
2022-12-30 16:34:44 +0100

    render tiles

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const board = document.querySelector("#board");
-const render = (height, width) => {};
+const render = (height, width) => {
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            board.appendChild(tile);
+            console.log(i, j);
+        }
+    }
+};
+render(9, 9);
