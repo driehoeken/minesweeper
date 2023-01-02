9021811dedbe3eb8af55f5292b55e20bf360b4e6
driehoeken <a.bednarski1203@gmail.com>
2023-01-02 22:06:04 +0100

    generating bombs positions

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+import { getRandomInt } from "./misc.js";
 const board = document.querySelector("#board");
-const render = (height, width) => {
+
+const generateBombsData = (height, width, bombsNum) => {
+    const bombs = [];
+    const bombsHelper = [];
+    let leftBombs = bombsNum;
+    while (leftBombs > 0) {
+        const bombPos = {
+            x: getRandomInt(width),
+            y: getRandomInt(height),
+        };
+        const bombHelp = `${bombPos.x}, ${bombPos.y}`;
+        if (!bombsHelper.includes(bombHelp)) {
+            bombs.push(bombPos);
+            bombsHelper.push(bombHelp);
+            leftBombs -= 1;
+        }
+    }
+    return bombs;
+};
+const renderTiles = (height, width) => {
     for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++) {
             const tile = document.createElement("div");
@@ -9,4 +29,4 @@ const render = (height, width) => {
         }
     }
 };
-render(9, 9);
+renderTiles(9, 9);
